db592fd2ed170238c07c0579112c1cc1510ff196
benlua <benlua73@gmail.com>
2025-05-09 21:57:28 +0800

    Initial Commit

---
+++ Reclaimr/ReclaimrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReclaimrApp.swift
+//  Reclaimr
+//
+//  Created by ben on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ReclaimrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReclaimrTests/ReclaimrTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReclaimrTests.swift
+//  ReclaimrTests
+//
+//  Created by ben on 9/5/25.
+//
+
+import Testing
+@testable import Reclaimr
+
+struct ReclaimrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
